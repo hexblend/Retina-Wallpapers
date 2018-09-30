801a7df5386e5d52ab865f54d67496ae188baac0
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-09-30 11:02:48 +0100

    Single image link

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Photos = props => (
     <React.Fragment>
         {props.getPhotos && props.getPhotos.map((photo) => {
             return (
-                <div key={photo.id} className="col-md-4">
-                    {photo.height >= 1600 && photo.width >= 2560 && <img src={photo.urls.small} alt={photo.urls.description} />}
-                </div>
+                    <div className="col-md-4">
+                        <Link to={{ pathname: `/photo/${photo.id}` }}>
+                            {photo.height >= 1600 && photo.width >= 2560 && <img src={photo.urls.small} alt={photo.urls.description} />}
+                        </Link>
+                    </div>
             );
         })}
     </React.Fragment>
