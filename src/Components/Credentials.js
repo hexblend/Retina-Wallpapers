352797fb7d5e3d6dcf978c837e9081152c48949d
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-10-09 13:52:48 +0100

    Final Touches before v1.0 release

--- src/Components/Credentials.js
+++ src/Components/Credentials.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Credentials = () => (
     <div className="Credentials-Component">
-        <p>An app by <a href="https://vladb.uk/" target="_blank">Vlad Bibire</a></p>
-        <p>Photo library by <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
+        <p>An app by <a href="https://vladb.uk/" target="_blank" rel="noopener noreferrer">Vlad Bibire</a></p>
+        <p>Photo library by <a href="https://unsplash.com/" target="_blank" rel="noopener noreferrer">Unsplash</a></p>
         <p>&copy; 2018 Retina Wallpapers</p>
     </div>
 );
--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -8,6 +8,7 @@ import Photos from './Photos';
 import Footer from './Footer';
 
 const API_KEY = '1686e47585b8407987d325464925639fa05f1195fca4b2c201d4dae923a7f274';
+let pageNumber = 1;
 
 class SearchPage extends Component {
     state = {
@@ -16,7 +17,7 @@ class SearchPage extends Component {
 
     componentWillMount = async () => {
         const Search = this.props.match.params.keyword;
-        const api_call = await fetch(`https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=1&query=${Search}&orientation=landscape&per_page=40&order_by=popular`);
+        const api_call = await fetch(`https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${this.state.page}&query=${Search}&orientation=landscape&per_page=40&order_by=popular`);
         const data = await api_call.json();
         this.setState({ photos: data.results });
     }
@@ -24,7 +25,7 @@ class SearchPage extends Component {
     getInput = () => {
         setTimeout(async () => {
             const newSearch = this.props.match.params.keyword;
-            const api_request = await fetch(`https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=1&query=${newSearch}&orientation=landscape&per_page=40&order_by=popular`);
+            const api_request = await fetch(`https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${this.state.page}&query=${newSearch}&orientation=landscape&per_page=40&order_by=popular`);
             const api_response = await api_request.json();
             this.setState({ photos: api_response.results });
         }, 100);
