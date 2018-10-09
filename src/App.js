d49223c7874522a7314c5a55deebea7ac64ac647
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-10-09 12:10:44 +0100

    Added credentials on Homepage

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ class App extends Component {
           <Titles />
           <Form />
         </div>
+        <div className="credentials">
+          <p>An app by <a href="vladb.uk">Vlad Bibire</a></p>
+          <p>Photo library by <a href="unsplash.com">Unsplash</a></p>
+          <p>&copy; 2018 Retina Wallpapers</p>
+        </div>
       </div>
     );
   }
